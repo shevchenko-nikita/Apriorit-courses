5eee122899a01ed98cac7fdae91e85fec7f94306
shevchenko-nikita <nicksevchenkos@gmail.com>
2024-03-10 17:10:06 +0200

    lection 2 - homework

---
+++ Lection2/code_review/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "MyVector.h"
+
+int main() {
+    MyVector<int> vec;
+
+    vec.push_back(10, "ten");
+    vec.push_back(5, "five");
+    vec.push_back(12, "twelve");
+    vec.push_back(100, "one hundred");
+
+
+    for(const auto& str : {"five", "ten", "twelve", "kek"}) {
+//        try {
+//            std::cout << vec[str] << std::endl;
+//        }
+//        catch (const std::exception& ex) {
+//            std::cout << ex.what() << '\n';
+//            exit(1);
+//        }
+        try {
+            std::pair<const int&, const std::string&> element = vec[1];
+            std::cout << element.first << ' ' << element.second << std::endl;
+        }
+        catch(const std::exception& ex) {
+            std::cout << ex.what() << std::endl;
+            exit(1);
+        }
+    }
+
+}
